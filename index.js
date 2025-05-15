d793737cb2599a9a0f81d855926583fa94cb85b5
Nikolai <krezi3927@gmail.com>
2025-05-15 17:07:04 +0900

    start test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // server.js
 const express = require("express");
 const cors = require("cors");
-const serverless = require("serverless-http");
+const axios = require("axios");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -14,8 +14,11 @@ app.get("/", (req, res) => {
   res.json({ message: "ok!" });
 });
 // Простой маршрут для проверки работы сервера
-app.get("/hello", (req, res) => {
-  res.json({ message: "ok!" });
+app.get("/getmanga", async (req, res) => {
+  const data = await axios
+    .get(`https://api.mangadex.org/manga`)
+    .then((res) => res.data);
+  res.json(data);
 
   res.send("Привет! Сервер на Node.js, Express и CORS работает.");
 });
