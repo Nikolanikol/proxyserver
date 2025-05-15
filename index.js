2d4192754ec0ef1c79455202bb99e32a8c310e4f
Nikolai <krezi3927@gmail.com>
2025-05-15 23:12:19 +0900

    test

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ const PORT = process.env.PORT | 3000;
 app.get("/", (req, res) => {
   res.send("Hello all working");
 });
-
+app.get("/manga", async (req, res) => {
+  try {
+    const responce = await axios
+      .get("https://api.mangadex.org/manga")
+      .then((res) => res.data);
+    res.send(responce);
+  } catch (error) {
+    console.log(error);
+  }
+});
 app.listen(PORT, () => {
   console.log("Server started on " + PORT);
 });
