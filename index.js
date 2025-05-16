7bcb89e39afb568c7ec3d7a5b80b00b90004b6d4
Nikolai <krezi3927@gmail.com>
2025-05-16 16:48:43 +0900

    test

--- index.js
+++ index.js
@@ -4,15 +4,36 @@ const express = require("express");
 
 const app = new express();
 const PORT = process.env.PORT | 3000;
+const BASEURL = `https://api.mangadex.org`;
 app.use(cors());
+//check endpoint
 app.get("/", (req, res) => {
   res.send("Hello all working");
 });
+//get mangai tems endpoint
 app.get("/manga", async (req, res) => {
   try {
     const responce = await axios
-      .get("https://api.mangadex.org/manga")
+      .get(BASEURL + "/manga", {
+        params: {
+          offset: 20,
+          limit: 20,
+        },
+      })
       .then((res) => res.data);
+    res.send(responce);
+  } catch (error) {
+    console.log(error);
+  }
+});
+//getcover img
+app.get("/cover", async (req, res) => {
+  try {
+    const { id } = req.query;
+
+    const responce = await axios(BASEURL + "/cover" + "/" + id).then(
+      (res) => res.data
+    );
     res.send(responce);
   } catch (error) {
     console.log(error);
