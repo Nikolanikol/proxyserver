17000aa1b5f9a1a6d0db08d8b50ab35b0e406d71
Nikolai <krezi3927@gmail.com>
2025-05-16 09:13:34 +0900

    test2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const app = new express();
 const PORT = process.env.PORT | 3000;
-
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello all working");
 });
